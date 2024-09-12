e510f1a743d40e3ab8918c8216e9f775e2297895
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-09-12 14:40:03 -0300

    feat: adiciona  keen-slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 
 import { HomeContainer, Product } from "../styles/pages/home";
-import { styled } from "../styles";
+import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
 import shirt1 from "../assets/shirts/shirt-1.png";
 import shirt2 from "../assets/shirts/shirt-2.png";
 import shirt3 from "../assets/shirts/shirt-3.png";
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
+    mode: "free",
+    slides: {
+      perView: 3,
+      spacing: 48,
+      perView: 2.5,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
-        <Image src={shirt1} width={520} height={480} alt="" />
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={shirt1} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
@@ -19,16 +29,16 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
-        <Image src={shirt2} width={520} height={480} alt="" />
+      <Product className="keen-slider__slide">
+        <Image src={shirt2} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
-        <Image src={shirt3} width={520} height={480} alt="" />
+      <Product className="keen-slider__slide">
+        <Image src={shirt3} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
