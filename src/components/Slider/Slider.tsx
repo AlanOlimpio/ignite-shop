97dfb0beb9ec9171424880dfef0e04de25bb2a08
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-10-22 10:36:26 -0300

    feat: adiciona Arrow no Slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,7 +1,13 @@
 "use client";
 import { useKeenSlider } from "keen-slider/react";
 import Image from "next/image";
-import { SliderContainer, Product, WrapperButton } from "./SliderStyles";
+import {
+  SliderContainer,
+  Product,
+  WrapperButton,
+  ArrowSlider,
+  WrapperNavigation,
+} from "./SliderStyles";
 import "keen-slider/keen-slider.min.css";
 import { priceFormatter } from "@/utils/formatter";
 import Link from "next/link";
@@ -18,11 +24,49 @@ interface SliderProps {
     price: number | null;
   }[];
 }
+function Arrow(props: any) {
+  const disabled = props.disabled ? " arrow--disabled" : "";
+  return (
+    <ArrowSlider
+      onClick={props.onClick}
+      className={`arrow ${
+        props.left ? "arrow--left" : "arrow--right"
+      } ${disabled}`}
+      xmlns="http://www.w3.org/2000/svg"
+      width="19"
+      height="34"
+      viewBox="0 0 19 34"
+      fill="none"
+    >
+      {props.left && (
+        <path
+          d="M18.0607 0.93934C17.4749 0.353553 16.5251 0.353553 15.9393 0.93934L0.93934 15.9393C0.353553 16.5251 0.353553 17.4749 0.93934 18.0607L15.9393 33.0607C16.5251 33.6464 17.4749 33.6464 18.0607 33.0607C18.6464 32.4749 18.6464 31.5251 18.0607 30.9393L4.12132 17L18.0607 3.06066C18.6464 2.47487 18.6464 1.52513 18.0607 0.93934Z"
+          fill="#C4C4CC"
+        />
+      )}
+      {!props.left && (
+        <path
+          d="M0.93934 0.93934C1.52513 0.353553 2.47487 0.353553 3.06066 0.93934L18.0607 15.9393C18.6464 16.5251 18.6464 17.4749 18.0607 18.0607L3.06066 33.0607C2.47487 33.6464 1.52513 33.6464 0.93934 33.0607C0.353553 32.4749 0.353553 31.5251 0.93934 30.9393L14.8787 17L0.93934 3.06066C0.353553 2.47487 0.353553 1.52513 0.93934 0.93934Z"
+          fill="#C4C4CC"
+        />
+      )}
+    </ArrowSlider>
+  );
+}
 
 function Slider({ products }: SliderProps) {
   const [hasCartList, setHasCartList] = useState<ProductInterfaceProps[]>([]);
-  const [sliderRef] = useKeenSlider<HTMLDivElement>({
+  const [loaded, setLoaded] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
     mode: "free",
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
     slides: {
       spacing: 48,
       perView: 2.5,
@@ -43,36 +87,60 @@ function Slider({ products }: SliderProps) {
   }, [cartList]);
 
   return (
-    <SliderContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => {
-        const hasCart = hasCartList.some((item) => item.id === product.id);
-        return (
-          <Link
-            href={`/product/${product.id}`}
-            key={product.id}
-            prefetch={false}
-          >
-            <Product key={product.name} className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
+    <WrapperNavigation>
+      <SliderContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          const hasCart = hasCartList.some((item) => item.id === product.id);
+          return (
+            <Link
+              href={`/product/${product.id}`}
+              key={product.id}
+              prefetch={false}
+            >
+              <Product key={product.name} className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-              <footer>
-                <div>
-                  <strong>{product.name}</strong>
-                  {product?.price && (
-                    <span>{priceFormatter.format(product.price)}</span>
+                <footer>
+                  <div>
+                    <strong>{product.name}</strong>
+                    {product?.price && (
+                      <span>{priceFormatter.format(product.price)}</span>
+                    )}
+                  </div>
+                  {hasCart && (
+                    <WrapperButton>
+                      <Handbag size={24} weight="bold" />
+                    </WrapperButton>
                   )}
-                </div>
-                {hasCart && (
-                  <WrapperButton>
-                    <Handbag size={24} weight="bold" />
-                  </WrapperButton>
-                )}
-              </footer>
-            </Product>
-          </Link>
-        );
-      })}
-    </SliderContainer>
+                </footer>
+              </Product>
+            </Link>
+          );
+        })}
+      </SliderContainer>
+      {loaded && instanceRef.current && (
+        <>
+          <Arrow
+            left
+            onClick={(e: any) =>
+              e.stopPropagation() || instanceRef.current?.prev()
+            }
+            disabled={currentSlide === 0}
+          />
+
+          <Arrow
+            onClick={(e: any) => {
+              e.stopPropagation() || instanceRef.current?.next();
+              console.log(currentSlide);
+            }}
+            disabled={
+              currentSlide ===
+              instanceRef.current.track.details.slides.length - 2
+            }
+          />
+        </>
+      )}
+    </WrapperNavigation>
   );
 }
 
--- src/components/Slider/SliderStyles.ts
+++ src/components/Slider/SliderStyles.ts
@@ -106,3 +106,39 @@ export const WrapperButton = styled("div", {
     borderRadius: "50%",
   },
 });
+
+export const WrapperNavigation = styled("div", {
+  position: "relative",
+  width: "100%",
+  maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
+  marginLeft: "auto",
+  minHeight: 656,
+  "@bp2": {
+    padding: "2.5rem 2rem 3rem",
+    minHeight: "auto",
+  },
+});
+
+export const ArrowSlider = styled("svg", {
+  "&.arrow": {
+    width: "30px",
+    height: "30",
+    position: " absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
+    fill: "#fff",
+    cursor: "pointer",
+  },
+  "&.arrow--left": {
+    left: "5px",
+  },
+
+  "&.arrow--right": {
+    left: "auto",
+    right: "5px",
+  },
+  "&.arrow--disabled": {
+    fill: "rgba(255, 255, 255, 0.5)",
+    cursor: "no-drop",
+  },
+});
